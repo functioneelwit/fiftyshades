7a268df9840da80c21eb6a6e17d45c33e2ec4a56
Functioneel Wit <info@functioneelwit.nl>
2016-07-15 14:44:16 +0200

    postform

--- functions.php
+++ functions.php
@@ -1,30 +1,56 @@
 <?php
-    
+
 function hex2rgb($hex)
 {
     $hex = str_replace("#", "", $hex);
- 
+
     if(strlen($hex) == 3)
     {
        $r = hexdec(substr($hex,0,1).substr($hex,0,1));
        $g = hexdec(substr($hex,1,1).substr($hex,1,1));
        $b = hexdec(substr($hex,2,1).substr($hex,2,1));
     }
-    else 
+    else
     {
        $r = hexdec(substr($hex,0,2));
        $g = hexdec(substr($hex,2,2));
        $b = hexdec(substr($hex,4,2));
     }
-    
+
     $rgb = array('r' => $r, 'g' => $g, 'b' => $b);
- 
+
     return $rgb; // returns an array with the rgb values
 }
 
 function format_name($string)
 {
     return strtolower(str_replace(' ', '-', $string));
+}
+
+function minify_hex($hex)
+{
+    if(
+
+      ( substr($hex,0,1) === substr($hex,1,1) )
+
+      &&
+
+      ( substr($hex,2,1) === substr($hex,3,1) )
+
+      &&
+
+      ( substr($hex,4,1) === substr($hex,5,1) )
+    )
+    {
+        $hex = substr($hex,0,1) . substr($hex,2,1)  . substr($hex,4,1);
+    }
+
+    return $hex;
+}
+
+function validate_hex($hex)
+{
+    return preg_match('/^[a-f0-9]{6}$/i', $hex);
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
- 
+
 include('functions.php');
- 
+
+if($_POST['input'])
+{
+    $input_hex = $_POST['input'];
+
+    if(validate_hex($input_hex))
+    {
+        header('location: ' . $input_hex);
+        exit;
+    }
+    else
+    {
+        $error = '#' . $input_hex . ' is no valid hex color value.. better yourself!';
+    }
+
+}
+
+
 if( $_GET['q'] )
 {
     $hex = strtoupper($_GET['q']);
@@ -10,31 +27,31 @@ if( $_GET['q'] )
     {
         $hex = $hex[0].$hex[0].$hex[1].$hex[1].$hex[2].$hex[2];
     }
-    
+
     $rgb = hex2rgb($hex);
-        
+
     if( $rgb['r'] === $rgb['g'] && $rgb['g'] === $rgb['b'] )
     {
-        
+
         $code = 100 - round((($rgb['r'] / 255) * 100));
-    
+
         if( $code < 10 )
         {
             $code = '0' . $code;
         }
-        
-        $class = '$color--gray-' . $code. ': #' . $hex . ';';
+
+        $class = '$color--gray-' . $code. ': #' . minify_hex($hex) . ';';
 
     }
     else
     {
-       
+
         $find_by_js = true;
-        
+
     }
-    
-    $input = '<input id="code" onClick="this.select();" value="' . @$class . '">';
-    
+
+    $input = '<input id="code" value="' . @$class . '">';
+
 }
 
 ?>
@@ -42,25 +59,45 @@ if( $_GET['q'] )
 <html>
 <head>
     <style>
-        
+
         html
         {
             text-align: center;
-            background-color: #<?php echo $hex; ?>;
+            background-color: #<?php echo $hex ? $hex : 'ddd' ?>;
             font-family: sans-serif;
         }
-        
-        input
+
+        form
+        {
+            padding: 30px;
+        }
+
+        input.input
+        {
+            width: 120px;
+            text-align: center;
+        }
+
+        input.input,
+        input.submit
+        {
+            font-size: 20px;
+            border-radius: 4px;
+            border: none;
+            padding: 5px 15px;
+        }
+
+        input#code
         {
             width: 50%;
-            margin-top: 45vh; 
-            font-size: 30px; 
-            padding: 10px; 
+            margin-top: 15vh;
+            font-size: 30px;
+            padding: 10px;
             text-align: center;
             border-radius: 8px;
             border: none;
         }
-        
+
         .compare
         {
             display: inline-block;
@@ -69,51 +106,83 @@ if( $_GET['q'] )
             padding: 30px;
             border-radius: 8px;
         }
-        
+
         .item
         {
-            float: left; 
+            float: left;
             width: 10%;
         }
-        
+
+        .error
+        {
+            display: inline-block;
+            background-color: red;
+            padding: 6px 12px;
+            color: white;
+            font-weight: bold;
+            border-radius: 8px;
+            margin: 5px;
+        }
+
     </style>
-    
+
     <script type="text/javascript" src="ntc.js"></script>
-        
+
 </head>
 <body>
-    
+
+<?php
+
+    if(isset($error))
+    {
+
+?>
+
+    <span class="error"><?php echo $error ?></span>
+
+<?php
+
+    }
+
+?>
+
+    <form method="POST">
+        <input type="text" class="input" name="input" value="<?php echo ( $input_hex ) ? $input_hex : $hex ?>" placeholder="XXXXXX"/>
+        <input type="submit" class="submit" value="Go!" />
+    </form>
+
+
     <?php echo $input; ?>
 
     <br />
 
-    <?php 
-        
+    <?php
+
     if(isset($find_by_js))
     {
 ?>
-    
+
     <span class="compare" id="compare"></span>
-    
+
     <script type="text/javascript">
 
         var n_match  = ntc.name("#<?php echo $hex; ?>");
-        var name =  n_match[1];
-    
+        var name = n_match[1];
+
         name = name.replace(" / ", "-");
         name = name.replace(" ", "-");
-        name = name.replace("/", "").toLowerCase();
-    
-        document.getElementById('code').value = '$color--' + name + ': #<?php echo $hex; ?>;';
+        name = name.toLowerCase();
+
+        document.getElementById('code').value = '$color--' + name + ': #<?php echo minify_hex($hex) ?>;';
 
     </script>
 
 <?php
-    
+
     }
-    
+
 ?>
 
-    
+
 </body>
 </html>
